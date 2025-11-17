1cabfdd6c6cefe40dc193a619d52a0a2041f799f
Dilraj Mukkar <dilrajm@live.com>
2025-11-17 02:16:58 -0500

    Made the level 1-3 buttons work, and the ? button now works with a popup, you can either click the x button or anywhere else on the screen to exit git push
    git push
    q

--- fruidlecode.js
+++ fruidlecode.js
@@ -1,7 +1,55 @@
+window.onload=function(){
+    //load the homepage first
+    innitGame();
+
+    //get all help buttons
+    const helpButtons=document.querySelectorAll(".help");
+    const modal= document.getElementById("HowToPlayModal");
+    const closeButton=document.getElementById("closeHowToPlay");
+
+    helpButtons.forEach(button=>{
+        button.onclick=()=>{
+             modal.style.display="flex";
+        };
+    });
+
+    closeButton.onclick=()=>{
+        modal.style.display="none";
+    };
+
+    window.onclick = (e) =>{
+        if(e.target===modal){
+            modal.style.display = "none";
+        }
+    };
+}
 function innitGame() {
-    
+    //homepage
+    showPage("Homepage")
 }
 
-function startLevel() {
-    
+function startlevel(level) {
+    if (level===0){
+        showPage("Homepage")
+    }
+    else{
+        showPage("Level_"+level);
+    }
 }
+
+/**
+ * This function for swithcing between visible pages
+ */
+function showPage(pageID){
+    //Hide pages
+    document.querySelectorAll(".page").forEach(page =>{
+        page.classList.remove("active");
+    } );
+    //Show page
+    document.getElementById(pageID).classList.add("active");
+}
+
+
+
+   
+
